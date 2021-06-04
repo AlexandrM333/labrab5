3cbd158e8e42c4f72d29d55426a5bb73f547979f
Your Name <Alex@gmail.com>
2021-06-04 05:26:17 +0300

    Задания

---
+++ Vipolnenie/zadan1.py
@@ -0,0 +1,6 @@
+
+
+m=input("Введите предложение")
+for i in range(len(m)):
+    if (i+1)%3==0:
+        print(m[i])
---
+++ Vipolnenie/zadan2.py
@@ -0,0 +1,10 @@
+s=input("Введите предложение")
+if s.find('чу')<0 or s.find('щу')<0:
+    print("Таких буквосочетаний нет")
+else:
+    if s.find('чу')<s.find('щу'):
+        print(s.find('чу'))
+    else:
+        print(s.find('щу'))
+
+
