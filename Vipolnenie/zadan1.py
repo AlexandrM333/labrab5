0ef93cde9ec45df9faea02fb8174c205837e2196
Your Name <Alex@gmail.com>
2021-06-11 10:20:08 +0300

    Задания

--- Vipolnenie/zadan1.py
+++ Vipolnenie/zadan1.py
@@ -1,6 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-
-m=input("Введите предложение")
+m = input("Введите предложение")
 for i in range(len(m)):
-    if (i+1)%3==0:
+    if (i+1)%3 == 0:
         print(m[i])
--- Vipolnenie/zadan2.py
+++ Vipolnenie/zadan2.py
@@ -1,8 +1,12 @@
-s=input("Введите предложение")
-if s.find('чу')<0 or s.find('щу')<0:
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+s = input("Введите предложение")
+
+if s.find('чу') < 0 or s.find('щу') < 0:
     print("Таких буквосочетаний нет")
 else:
-    if s.find('чу')<s.find('щу'):
+    if s.find('чу') < s.find('щу'):
         print(s.find('чу'))
     else:
         print(s.find('щу'))
